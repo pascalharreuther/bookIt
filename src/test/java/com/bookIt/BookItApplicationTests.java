1734757a0ebd9cdc102a6fa766060578234d1948
pascal.harreuther <pascal.harreuther@people.valantic.com>
2022-02-14 17:21:06 +0100

    Initial commit

---
+++ src/test/java/com/bookIt/BookItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
