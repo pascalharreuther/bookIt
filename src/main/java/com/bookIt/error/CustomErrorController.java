146a24b7c9c3039d979826bf6f2538092bdb1db9
pascal.harreuther <pascal.harreuther@people.valantic.com>
2022-02-15 10:33:50 +0100

    add FaviconConfiguration

--- src/main/java/com/bookIt/error/CustomErrorController.java
+++ src/main/java/com/bookIt/error/CustomErrorController.java
@@ -9,18 +9,24 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Controller for custom errors
+ */
 @Controller
-public class CustomeErrorController implements ErrorController {
+public class CustomErrorController implements ErrorController {
 
+    /**
+     * Handle every request with "/error" (server.error.path=/error)
+     * @param request
+     * @return modelAndView
+     */
     @RequestMapping("/error")
     public ModelAndView handleError(HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView("/error/default");
         modelAndView.addObject("errorStatusCode", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
         modelAndView.addObject("errorRequestUri", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
         modelAndView.addObject("errorMessage", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
-        modelAndView.addObject("errorExceptionType", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
         modelAndView.addObject("errorException", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
-        modelAndView.addObject("errorServletName", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
         return modelAndView;
     }
 }
