a2decbe855f23d78a20aff900788c00b51d24205
pascal.harreuther <pascal.harreuther@people.valantic.com>
2022-02-15 10:39:14 +0100

    add FaviconConfiguration

---
+++ src/main/java/com/bookIt/config/GlobalAttributesController.java
@@ -0,0 +1,25 @@
+package com.bookIt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+/**
+ * Controller for global attributes
+ */
+@ControllerAdvice
+public class GlobalAttributesController {
+
+    @Value("${bookIt.title:bookIt}")
+    private String bookItTitle;
+
+    /**
+     * Add global attributes to global thymeleaf model for every html page
+     * @param model
+     */
+    @ModelAttribute
+    public void addGlobalAttributes(Model model){
+        model.addAttribute("bookItTitle", bookItTitle);
+    }
+}
---
+++ src/main/java/com/bookIt/index/IndexController.java
@@ -0,0 +1,17 @@
+package com.bookIt.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for index
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView handleIndex(){
+        return new ModelAndView("index");
+    }
+}
