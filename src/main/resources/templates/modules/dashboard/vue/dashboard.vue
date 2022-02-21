869029b736fda8003f39ac92f0c6c5b7f1eedb5a
pascal.harreuther <pascal.harreuther@people.valantic.com>
2022-02-21 22:37:36 +0100

    add seminars to dashboard

--- src/main/resources/templates/modules/dashboard/vue/dashboard.vue
+++ src/main/resources/templates/modules/dashboard/vue/dashboard.vue
@@ -25,28 +25,5 @@
         </div>
       </v-expand-transition>
     </v-card>
-    <v-card mx-auto>
-      <v-card-title>
-        <span>Seminarname</span>
-      </v-card-title>
-      <v-divider></v-divider>
-      <v-card-text>
-        <v-row>
-          <v-col cols="4">12.04.2022 - 15.04.2022</v-col>
-          <v-col cols="4" color="success--text">2/18</v-col>
-          <v-col cols="4"></v-col>
-        </v-row>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn @click="show = !show">
-          <span>More Informations</span>
-        </v-btn>
-      </v-card-actions>
-      <v-expand-transition>
-        <div v-show="show = !show">
-          testtext
-        </div>
-      </v-expand-transition>
-    </v-card>
   </v-container>
 </template>
